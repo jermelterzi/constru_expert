819e41c50b281f2ba61154e1ed32e0ebd0cb1240
João Victor Terzi <joao.terzi@afixcode.com.br>
2024-02-09 21:38:52 -0300

    Feat: Creating construction feature page

---
+++ lib/src/app.dart
@@ -0,0 +1,18 @@
+import 'package:constru_expert/src/features/construction/presentation/pages/constructions_page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const ConstructionsPage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
